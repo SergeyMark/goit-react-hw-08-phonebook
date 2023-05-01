c992ee233e3ecdec241c8ee06e6b5c5a368d635d
Sergey <110375004+SergeyMark@users.noreply.github.com>
2023-05-01 17:58:59 +0300

    add end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { ContactForm } from './ContactForm/ContactForm';
-import { ContactList } from './ContactList/ContactList';
-import { ContactFilter } from './ContactFilter/ContactFilter';
+import { Route, Routes } from "react-router-dom";
 import { fetchContacts } from 'redux/contacts/operations';
 import { selectError, selectIsLoading } from "redux/contacts/contact-selector";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import css from './App.module.css';
+import { RestrictedRoute } from "./RestrictedRoute";
+import { PrivateRoute } from "./PrivateRoute";
+import { Layout } from "./Layout";
+import Home from "page/Home";
+import Registr from "page/Register";
+import Login from "page/Login";
+import Contacts from "page/Contacts";
+
+
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -18,16 +24,20 @@ export const App = () => {
   }, [dispatch])
 
   return (
-    <div className={css.container}>
-      <h1>Phonebook</h1>
-      <ContactForm/>
-      <ContactFilter/>
-      {(IsLoading && !error && (
-        <>
-          <br/>
-          <p>in progress... Wait</p>
-        </>
-      )) || <ContactList/>}
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home/>}/>
+        <Route path="/register" element={
+          <RestrictedRoute redirectTo="/contacts" component={<Registr/>}/>
+        }/>
+        <Route path="/login" element={
+          <RestrictedRoute redirectTo="/contacts" component={<Login/>}/>
+        }/>
+        <Route path="/contacts" element={
+          <PrivateRoute redirectTo="/login" component={<Contacts/>}/>
+        }/>
+        <Route path="*" navigateTo="/" element={<Home/>} />
+      </Route>
+    </Routes>
   );
 };
--- src/page/Contacts.jsx
+++ src/page/Contacts.jsx
@@ -1,14 +1,34 @@
+import React from "react";
 import { ContactForm } from "components/ContactForm/ContactForm";
 import { ContactFilter } from "components/ContactFilter/ContactFilter";
 import { ContactList } from "components/ContactList/ContactList";
+import { fetchContacts } from "redux/contacts/operations";
+import { selectError, selectIsLoading } from "redux/contacts/contact-selector";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 
 
 export default function Contacts() {
+    const dispatch = useDispatch();
+    const isLoading = useSelector(selectIsLoading)
+    const error = useSelector(selectError)
+
+    useEffect(()=>{
+        dispatch(fetchContacts());
+    },[dispatch])
+
     return(
         <div>
+            <h1>Phonebook</h1>
             <ContactForm/>
             <ContactFilter/>
-            <ContactList/>
+            {(isLoading && !error && (
+                <>
+                    <br/>
+                    <p>in progress... Wait</p>
+                </>
+            )) || <ContactList/>}
         </div>
     )
 }
